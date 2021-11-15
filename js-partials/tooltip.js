d986f096584301608fbd926c598ec99338e30716
dannyfrelink <danny-frelink@hotmail.com>
2021-11-15 13:55:30 +0100

    styling naar CSS verplaatsen

--- js-partials/tooltip.js
+++ js-partials/tooltip.js
@@ -3,12 +3,7 @@ function createTooltip() {
         .select("#scatter-line")
         .append("div")
         .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px");
+        .attr("class", "tooltip");
 
     return tooltip;
 }
