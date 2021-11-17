c2960663ec8d92e9f3ca3b100577ab1256bbf8a4
dannyfrelink <danny-frelink@hotmail.com>
2021-11-17 15:26:53 +0100

    apart document voor mouse hover functies

--- js-partials/update.js
+++ js-partials/update.js
@@ -1,20 +1,5 @@
 import { g } from './basicVars.js';
-import { createTooltip } from './tooltip.js';
-const tooltip = createTooltip();
-
-// Hover functions
-const mouseover = function () {
-    tooltip.style('opacity', 1);
-};
-const mousemove = function (d) {
-    tooltip
-        .html(`${d.target.__data__.currency}: ${d.target.__data__.value}`)
-        .style('left', d3.pointer(event, this)[0] + 90 + 'px')
-        .style('top', d3.pointer(event, this)[1] + 130 + 'px');
-};
-const mouseleave = function () {
-    tooltip.style("opacity", 0);
-};
+import { mouseover, mousemove, mouseleave } from './hover.js'
 
 function updateChart(continent, scales) {
     const { xScale, yScale, g_xAxis, g_yAxis, xAxis, yAxis } = scales;
